452620c815761ea70db3ac2d59df31497e88d30b
Qunainb <waniqunain@gmail.com>
2025-06-26 16:17:05 +0530

    Consuming the store also added an input field

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -5,6 +5,8 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import enUS from "date-fns/locale/en-US";
+import { reminderStore } from "../store";
+import { useState } from "react";
 
 const locales = {
   "en-US": enUS,
@@ -19,26 +21,62 @@ const localizer = dateFnsLocalizer({
 });
 
 export const MyCalendar = () => {
-  const events = [
-    {
-      title: "Test event",
-      start: new Date(2025, 5, 6),
-      end: new Date(2025, 5, 6),
-      allDay: true,
-    },
-  ];
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [remindertext, setReminderText] = useState("");
+
+  const reminders = reminderStore((state) => state.reminders);
+  const addReminder = reminderStore((state) => state.addReminder);
+
+  function handleClick(slot) {
+    setSelectedDate(slot.start);
+  }
+
+  const reminderEvents = Object.entries(reminders).flatMap(
+    ([dateKey, reminderText]) =>
+      reminderText.map((text) => ({
+        title: text,
+        start: new Date(dateKey),
+        end: new Date(dateKey),
+        allDay: true,
+      }))
+  );
 
   return (
     <div>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={reminderEvents}
         startAccessor="start"
         endAccessor="end"
         views={["month"]}
         toolbar={false}
+        selectable={true}
+        popup={true}
+        onSelectSlot={handleClick}
         style={{ height: 500, margin: "50px" }}
       />
+
+      {selectedDate && (
+        <div>
+          <input
+            type="text"
+            value={remindertext}
+            onChange={(e) => setReminderText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (!selectedDate || !remindertext) return;
+
+              const formattedDate = format(selectedDate, "yyyy-MM-dd");
+              addReminder(formattedDate, remindertext);
+              setReminderText("");
+              setSelectedDate(null);
+            }}
+          >
+            Save
+          </button>
+        </div>
+      )}
     </div>
   );
 };
